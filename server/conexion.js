76f0870a7fa3fa60e5623891f3b0612fb98854f8
Santiago <santytorres879@gmail.com>
2025-04-26 20:49:20 -0500

    conexion

---
+++ server/conexion.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectionDb = async () => {
+        const conexion = "mongodb+srv://SanCode:FOpldaLQgqNTxhYh@cluster0.tsmwijl.mongodb.net/sistema_chatapi?retryWrites=true&w=majority&appName=Cluster0"
+  try {
+
+    const connectDb = await mongoose.connect(conexion);
+    console.log(
+      'Connection established',
+      connectDb.connection.name,
+      connectDb.connection.host);
+  } catch (err) {
+    console.log('Fallo en la conexion: ' + err);
+  };
+};
+
+connectionDb();
