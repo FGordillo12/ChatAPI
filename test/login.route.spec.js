15a240dd212e9ac268715f119c3c3ae925267ae1
FGordillo12 <egordillo@ucundinamarca.edu.co>
2025-05-22 15:59:44 -0500

    prueba unitaria de login

---
+++ test/login.route.spec.js
@@ -0,0 +1,37 @@
+import supertest from 'supertest';
+import { app } from '../server';
+
+describe('Pruebas sobre la API', () => {
+
+    describe('POST /api/login', () => {
+        it('El login funciona correctamente', async () => {
+
+            const metodoPOST = 
+            {
+                email:"cuenta99cuenta9@gmail.com",
+                password:"Hola12345"
+            };
+
+            const response = await supertest(app).post('/api/login').send(metodoPOST);
+            expect(response.status).toBe(200);
+            expect(response.body).toHaveProperty('token');
+
+        });
+
+        it('El login falla con credenciales incorrectas', async () => {
+
+            const metodoPOST = 
+            {
+                email:"cuenta99cuenta9@gmail.com",
+                password:"Hola12345"
+            };
+            
+            const response = await supertest(app).post('/api/login').send(metodoPOST);
+            expect(response.status).toBe(401);
+            expect(response.body).toHaveProperty('mensaje de error');
+
+        });
+
+    });
+
+});
