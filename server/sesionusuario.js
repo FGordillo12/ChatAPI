23097fa72cd4cb09205df2874a3043b5e762d4f7
Stiven <usg210802@outlook.com>
2025-05-25 15:12:13 -0500

    Cambio en los nombres de las pruebas, creacion de jwt para la validacion de los usuarios

--- server/sesionusuario.js
+++ server/sesionusuario.js
@@ -1,21 +1,39 @@
 import Usuario from "../models/usuarios.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const iniciarSesion = async (req, res) => {
-  const { email_login, password_login } = req.body;
+  const { email, password } = req.body;
 
   try {
-    const usuario = await Usuario.findOne({ email: email_login });
+    const usuario = await Usuario.findOne({ email });
     if (!usuario) {
       return res.status(401).json({ message: "Credenciales inválidas" });
     }
 
-    const passwordValido = await bcrypt.compare(password_login, usuario.password);
+    const passwordValido = await bcrypt.compare(password, usuario.password);
     if (!passwordValido) {
       return res.status(401).json({ message: "Credenciales inválidas" });
     }
 
-    res.status(200).json({ message: "Inicio de sesión exitoso", usuario });
+    // ✅ Generar el token JWT
+    const token = jwt.sign(
+      { id: usuario._id, email: usuario.email, type: usuario.type },
+      process.env.JWT_SECRET || "clave-secreta", // reemplázalo por una env segura
+      { expiresIn: "1h" }
+    );
+  //DEVOLVER EL TOKEN
+    res.status(200).json({
+      message: "Inicio de sesión exitoso",
+      token,
+      usuario: {
+        id: usuario._id,
+        nombreCompleto: usuario.nombreCompleto,
+        email: usuario.email,
+        type: usuario.type
+      }
+    });
+
   } catch (error) {
     console.error("Error al iniciar sesión:", error);
     res.status(500).json({ message: "Error en el servidor" });
--- test/registro.js
+++ test/registro.js
@@ -3,18 +3,19 @@ import { app } from '../server';
 describe('Pruebas sobre la API', () => {
   describe('POST /api/registro', () =>{
 
-    it('El registro esta bien', async () =>{
+    it('Prueba de registro', async () =>{
 
       const metodoPOST = 
       {
-        nombreCompleto: "Jose Alfonso", 
-        email:"cuenta99cuenta9@gmail.com", 
-        password:"Alfonso12@",
+        nombreCompleto: "Prueba Uno", 
+        email:"pruebauno@gmail.com", 
+        password:"Hola12345*",
         type:"Usuario" 
       }
 
       const response = await supertest(app).post('/api/registro').send(metodoPOST)
       expect(response.status).toBe(201);
+      console.log('RESPUESTA DEL SERVIDOR:', response.body); 
     
     })
 
