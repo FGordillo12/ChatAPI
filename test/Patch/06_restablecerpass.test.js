cddff3a2b4e3d7175fd85855f2800e807ee06b8a
Santiago <santytorres879@gmail.com>
2025-06-04 12:14:18 -0500

    Prueba 6 restablecer contraseña

---
+++ test/Patch/06_restablecerpass.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Restablecer contraseña', () => {
+  test('PATCH /restablecer_password/:token', async () => {
+    const response = await request(app)
+      .patch('/restablecer_password/token123')
+      .send({ nuevaPassword: 'NuevaPass123' });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.success).toBe(true);
+  });
+});
---
+++ test/Post/05_recuperarPassword.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Recuperar contraseña', () => {
+  test('POST /recuperar_password', async () => {
+    const response = await request(app)
+      .post('/recuperar_password')
+      .send({ email: 'test@correo.com' });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toBeDefined();
+  });
+});
+
