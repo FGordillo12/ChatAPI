43709eeae752dbc052d71f0ccc308ea19095ee59
Santiago <santytorres879@gmail.com>
2025-06-04 12:19:05 -0500

    buscar mensaje

---
+++ test/Get/10_buscarmensaje.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Mensajes entre usuarios', () => {
+  test('GET /mensajes/:usuario1/:usuario2', async () => {
+    const response = await request(app)
+      .get('/mensajes/userA/userB');
+
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+});
