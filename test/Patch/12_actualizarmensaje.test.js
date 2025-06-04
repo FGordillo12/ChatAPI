1a4b2ae302b34d30d811428d2272d6353fdf97e7
Santiago <santytorres879@gmail.com>
2025-06-04 12:23:26 -0500

    Prueba 12 actualizar mensjae

---
+++ test/Patch/12_actualizarmensaje.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Actualizar mensaje', () => {
+  test('PATCH /mensajes/:id', async () => {
+    const response = await request(app)
+      .patch('/mensajes/12345')
+      .send({ leido: true });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.updated).toBe(true);
+  });
+});
