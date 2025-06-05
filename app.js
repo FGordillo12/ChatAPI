d5c862bb1ee2962609cf1bf8f7967fca70f825c0
FGordillo12 <egordillo@ucundinamarca.edu.co>
2025-06-05 17:53:29 -0500

    documentacion de archivos "app" "index"

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Importaciones de express, cookie-parser, cors y las rutas
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -5,21 +6,21 @@ import { routerUsuarios } from "./server/routes/usuariosRoutes.js";
 import { routerPagina } from "./server/routes/paginaRoutes.js";
 import { routerGraficos } from "./server/routes/graficosRoutes.js";
 
-const app = express();
+const app = express(); //Se crea una instancia de express
 
-const corsOptions = {
-  origin: 'http://localhost:5173',
+const corsOptions = { //Importacion de cors para permitir peticiones desde el frontend
+  origin: 'http://localhost:5173', //Frontend URL
   allowedHeaders: 'Content-Type,Authorization',
-  credentials: true
+  credentials: true // Permitir cookies y credenciales
 };
 
-app.use(cookieParser());
-app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cookieParser()); // Middleware para parsear cookies en las solicitudes
+app.use(express.json()); // Middleware para parsear JSON
+app.use(cors(corsOptions)); // Middleware para habilitar CORS con opciones definidas
 
-// Rutas
-app.use('/api/', routerUsuarios);
-app.use('/api/', routerPagina);
-app.use('/api/', routerGraficos);
+// Rutas importadas y asignadas a la aplicación
+app.use('/api/', routerUsuarios); // Ruta para usuarios
+app.use('/api/', routerPagina); // Ruta para páginas
+app.use('/api/', routerGraficos); // Ruta para gráficos
 
-export default app;
+export default app; // Exporta la aplicación para ser utilizada en otros módulos
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+//Importaciones de controladores para las rutas de gráficos
 import 'dotenv/config';
 import { createServer } from "node:http";
 import connectiondDb from './server/database/conexion.js';
 import app from './app.js';
 import { setupSocket } from './server/mensajes/conexionSockets.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; //Puerto del servidor, por defecto 3000
 
 // Crear servidor HTTP a partir de la app de Express
 const server = createServer(app);
@@ -21,10 +22,10 @@ const startServer = async () => {
         console.log(`Servidor escuchando en http://localhost:${PORT}`);
       });
     }
-  } catch (error) {
+  } catch (error) { //Muestra errores en consola si falla la conexión a la base de datos o al iniciar el servidor
     console.error('Error al iniciar el servidor:', error);
-    process.exit(1);
+    process.exit(1); //Fin del proceso con error
   }
 };
 
-startServer();
+startServer(); // Inicia el servidor y la conexión a la base de datos
